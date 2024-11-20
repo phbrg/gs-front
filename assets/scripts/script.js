7c1a3cc65106c2e3b3a6d3dbb2f32a230d42bcfd
phbrg <xomaicrocefalia123@gmail.com>
2024-11-20 19:07:14 -0300

    finished login page, styles and script

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,21 +1,45 @@
-const btnForm = document.querySelector('#btnForm');
+const btnContato = document.querySelector('#btnContato');
 
-btnForm.addEventListener('click', (e) => {
-  e.preventDefault();
+if(btnContato) {
+  btnContato.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    const elements = {
+      nome: document.querySelector('#nome'),
+      email: document.querySelector('#email'),
+      assunto: document.querySelector('#assunto')
+    }
   
-  const elements = {
-    nome: document.querySelector('#nome'),
-    email: document.querySelector('#email'),
-    assunto: document.querySelector('#assunto')
-  }
+    if(!elements.nome.value || !elements.email.value || !elements.assunto.value || elements.nome.value == '' || elements.email.value == '' || elements.assunto.value == '') {
+      sendAlert('Erro!', 'Você precisa preencher os campos antes de enviar!');
+    } else {
+      sendAlert('Mensagem enviada!', `Nome: ${elements.nome.value} <br/> Email: ${elements.email.value} <br/> Assunto: ${elements.assunto.value} `);
+    }
+  });
+}
 
-  if(!elements.nome.value || !elements.email.value || !elements.assunto.value || elements.nome.value == '' || elements.email.value == '' || elements.assunto.value == '') {
-    alert('Você precisa preencher os campos antes de enviar!')
-  } else {
-    sendAlert('Mensagem enviada!', `Nome: ${elements.nome.value} <br/> Email: ${elements.email.value} <br/> Assunto: ${elements.assunto.value} `)
-  }
-});
+const btnLogin = document.querySelector('#btnLogin');
 
+if(btnLogin) {
+  btnLogin.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    const elements = {
+      username: document.querySelector('#username'),
+      password: document.querySelector('#password')
+    }
+  
+    if(!elements.username.value || !elements.password.value || elements.username.value == '' || elements.password.value == '') {
+      sendAlert('Erro!', 'Você precisa preencher os campos antes de enviar!');
+    } else if(elements.username.value == 'admin' || elements.password.value == 'admin') {
+      sendAlert('Sucesso!', 'Login efetuado com sucesso.')
+    } else {
+      sendAlert('Erro!', 'Credenciais invalidas.');
+    }
+  });
+}
+ 
+// preferi fazer o meu alert 😅
 function sendAlert(title, msg) {
   const notifyDiv = document.querySelector('#notify');
   const notifyTitle = document.querySelector('.notifyTitle');
